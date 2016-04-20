b302ebf7ce68b6abcb46e6107f4e69bba5d8e376
bloodyKnuckles <treenopie@gmail.com>
2016-04-20 12:12:26 -0500

    Added types and conditional parameters, and updated README.

--- README.md
+++ README.md
@@ -5,25 +5,39 @@ Designed to work with the patch object returned by the [virtual-dom](https://git
 javascript:
 ```
 var patchView = require('virtual-dom-patch-viewer')
+
 // get patch, then...
 patchView(patch)
 ```
 
 console:
 ```
-8 REMOVE STYLE undefined
-10.0 PROPS BODY Object {attributes: Object}
-10.1 INSERT null VirtualText {text: "↵  "}
-12 PROPS DIV Object {attributes: Object}
-13 PROPS A Object {attributes: Object}
-16 PROPS A Object {attributes: Object}
+18 PROPS H2 VirtualNode {...} Object {...}
+19 VTEXT undefined VirtualText {...} VirtualText {...}
+20 REMOVE SPAN VirtualNode {...} undefined
+23 PROPS DIV VirtualNode {...} Object {...}
+24 VTEXT undefined VirtualText {...} VirtualText {...}
+25 REMOVE BUTTON VirtualNode {...} undefined
+27 REMOVE undefined VirtualText {...} undefined
 ```
 
-Optionally you can limit the report to one or more particular tags.
+Optionally you can limit the report to particular types or tags.
 
 ```
+// only show virtual patches that affect DIV tags
 patchView(patch, 'div')
-patchView(patch, ['div', 'h2'])
+
+// show either DIV or H2 tags or patches with type REMOVE
+patchView(patch, ['div', 'h2', 'remove'])
+
+// only show patches that are type REMOVE and affect SCRIPT tags
+patchView(patch, {type:'remove', tag:'script'})
+
+// show patches that are type REMOVE or INSERT, and affect SCRIPT tags
+patchView(patch, {type:['remove','insert'], tag:'script'})
+
+// show patches that are type PROPS, and affect DIV or ANCHOR tags
+patchView(patch, {type:'props', tag:['div','a']})
 ```
 
 ### license
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
-var actions = ['NONE', 'VTEXT', 'VNODE', 'WIDGET', 'PROPS', 'ORDER', 'INSERT', 'REMOVE', 'THUNK']
+var types = ['NONE', 'VTEXT', 'VNODE', 'WIDGET', 'PROPS', 'ORDER', 'INSERT', 'REMOVE', 'THUNK']
 
-module.exports = function patchReport (patch, showtag) {
-  if ( Array.isArray(showtag) ) {
-    showtag = showtag.map(function (tag) {
-      return tag.toLowerCase()
+module.exports = function patchReport (patch, opts) {
+
+  if ( Array.isArray(opts) ) {
+    opts = uCArray(opts)
+  }
+  else if ( 'object' === typeof opts ) {
+    Object.keys(opts).forEach(function (key) {
+      if ( Array.isArray(opts[key]) ) {
+        opts[key] = uCArray(opts[key])
+      }
     })
   }
+
   Object.keys(patch).forEach(function (key) {
-    if ( 'a' === key && !showtag ) {
+    if ( 'a' === key && !opts ) {
       console.log('a', patch[key])
     }
     else if ( Array.isArray(patch[key]) ) {
@@ -21,14 +28,54 @@ module.exports = function patchReport (patch, showtag) {
   })
 
   function report (ii, vpatch) {
-    if ( !showtag || (
-      vpatch.vNode && vpatch.vNode.tagName
+    var tagExists = vpatch.vNode && vpatch.vNode.tagName
+    var tagName = tagExists && vpatch.vNode.tagName.toUpperCase()
+
+    if ( !opts ||
+      (
+        'string' === typeof opts
         && (
-          ('string' === typeof showtag && showtag.toLowerCase() === vpatch.vNode.tagName.toLowerCase())
-          || (Array.isArray(showtag) && -1 !== showtag.indexOf(vpatch.vNode.tagName.toLowerCase()))
+          tagExists && opts.toUpperCase() === tagName
+          || opts.toUpperCase() === types[vpatch.type]
         )
-    ) ) {
-      console.log(ii, actions[vpatch.type], vpatch.vNode && vpatch.vNode.tagName, vpatch.vNode, vpatch.patch)
+      )
+      || (
+        Array.isArray(opts)
+        && (
+          tagExists && -1 !== opts.indexOf(tagName)
+          || -1 !== opts.indexOf(types[vpatch.type])
+        )
+      )
+      || (
+        'object' === typeof opts && (opts.type || opts.tag)
+        && (
+          (
+            !opts.type
+            || (
+              ('string' === typeof opts.type && opts.type.toUpperCase() === types[vpatch.type])
+              || (Array.isArray(opts.type) && -1 !== opts.type.indexOf(types[vpatch.type]))
+            )
+          )
+          && (
+            !opts.tag
+            || (
+              tagExists && (
+                ('string' === typeof opts.tag && opts.tag.toUpperCase() === tagName)
+                || (Array.isArray(opts.tag) && -1 !== opts.tag.indexOf(tagName))
+              )
+            )
+          )
+        )
+      )
+    ) {
+      console.log(ii, types[vpatch.type], vpatch.vNode && vpatch.vNode.tagName, vpatch.vNode, vpatch.patch)
     }
   }
+
+  function uCArray (arr) {
+    return arr.map(function (elem) {
+      return elem.toUpperCase()
+    })
+  }
+
 }
